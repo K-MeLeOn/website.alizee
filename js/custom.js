6ecb014fd27fe0bbe40eb5d3df6fb9537c9c8dd9
Nathan Meynaud <n.meynaud@bucci-industries.com>
2021-01-13 01:25:27 +0100

    Add home

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,9 @@
-!(function(){
+function myScript(){
     var includes = $('[data-include]');
     jQuery.each(includes, function(){
         var file = 'shared/' + $(this).data('include') + '.html';
         $(this).load(file);
     });
-});
+
+    $("#msgid").html("This is Hello World by JQuery");
+};
